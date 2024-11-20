d4e05a14a7f6a1eb44a89c21d959509e04979fbb
call-me-mac <maharajanravi95@gmail.com>
2024-11-20 18:18:32 +0530

    github added and commited

---
+++ src/test/java/com/github/publish/publisher/PublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.publish.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
